2e5df61184154bfd7a9e7bb73526d4241569865b
ondercraw <ungimca@gmail.com>
2022-04-16 16:17:34 -0300

    tpfinalizado

--- src/mostrarMenu.c
+++ src/mostrarMenu.c
@@ -23,6 +23,11 @@ int mostrarMenu(int opcionIngresada)
 return opcionIngresada;
 }
 
+/// @fn float mostrarMenu(int)
+/// @brief Se encarga de mostrar el menu de opciones.
+/// @param opcionIngresada
+/// @return la opcion ingresada del menu
+
 float mostrarResultados(float km, float precioAerolineas, float precioLatam, float descuentoA, float interesA, float precioConBitcoinA, float precioUnitarioA, float descuentoL, float interesL, float precioConBitcoinL, float precioUnitarioL, float diferencia)
 {
 
@@ -30,16 +35,32 @@ float mostrarResultados(float km, float precioAerolineas, float precioLatam, flo
 	            "\n\nPrecio Aerolineas: $ %.2f"
 	            "\na) Precio con tarjeta de debito: $ %.2f"
 	            "\nb) Precio con tarjeta de credito: $ %.2f"
-	            "\nc) Precio pagando con bitcoin: $ %.2f"
+	            "\nc) Precio pagando con bitcoin: $ %.7f"
 	            "\nd) Mostrar precio unitario: $ %.2f"
 	            "\n\nPrecio Latam: $ %.2f"
 	            "\na) Precio con tarjeta de debito: $ %.2f"
 	            "\nb) Precio con tarjeta de credito: $ %.2f"
-	            "\nc) Precio pagando con bitcoin: $ %.2f"
+	            "\nc) Precio pagando con bitcoin: $ %.7f"
 	            "\nd) Mostrar precio unitario: $ %.2f"
 	            "\n\n Diferencia: %.2f", km, precioAerolineas, descuentoA, interesA, precioConBitcoinA, precioUnitarioA, precioLatam, descuentoL, interesL, precioConBitcoinL, precioUnitarioL, diferencia);
 
 	return 0;
 }
+
+/// @fn float mostrarResultados(float , float , float , float , float , float , float , float , float , float , float , float)
+/// @brief Se encarga de mostrar los resultados de todos los calculos.
+/// @param km
+/// @param precioAerolineas
+/// @param precioLatam
+/// @param descuentoA
+/// @param interesA
+/// @param precioConBitcoinA
+/// @param precioUnitarioA
+/// @param descuentoL
+/// @param interesL
+/// @param precioConBitcoinL
+/// @param precioUnitarioL
+/// @param diferencia
+/// @return todos los precios ya calculados
 
 
