59b47f857e8aefae11afad6db8a10242fe738d1d
ondercraw <ungimca@gmail.com>
2022-06-11 21:28:45 -0300

    tpfin3

---
+++ inputs.h
@@ -0,0 +1,19 @@
+/*
+ * inputs.h
+ *
+ *  Created on: 11 jun. 2022
+ *      Author: ungim
+ */
+
+
+#ifndef INPUTSTP_H_
+#define INPUTSTP_H_
+
+
+void rellenarInt(int* variableRecibida, char textoAMostrar[]);
+
+void rellenarFloat(float* variableRecibida, char textoAMostrar[]);
+
+void rellenarArray(char arrayRecibido[], char textoAMostrar[]);
+
+#endif /* INPUTSTP_H_ */
